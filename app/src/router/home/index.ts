9d6bf744d72424526ca0b46cbe608c14e7586650
Christopher Kelley <chris@cranberry.ink>
2019-05-18 16:09:20 -0400

    Style grid and make way for data entry

--- app/src/router/home/index.ts
+++ app/src/router/home/index.ts
@@ -11,6 +11,9 @@ export default class Home extends Vue {
 
   public grid: Array<Array<any>> = null as any;
 
+  public active: boolean = false;
+  public activeData: null | [ number, number ] = null;
+
   public created ( ): void {
     const grid: Array<Array<any>> = new Array(this.years);
     for (let year = 0; year < this.years; ++year ) {
@@ -18,7 +21,7 @@ export default class Home extends Vue {
       for (let week = 0; week < 52; ++week) {
         grid[year][week] = {
           active: false,
-          content: false,
+          content: null,
           _id: null,
           year, week
         };
@@ -27,9 +30,8 @@ export default class Home extends Vue {
     this.$set(this, "grid", grid);
     this.$store.http.get("/entries").then(( { data }: any ): void => {
       for (const entry of data) {
-        const year = this.yearOf(entry.date);
-        const week = this.weekOf(entry.date);
-        Object.assign(this.grid[year][week], entry);
+        const m = entry.date.match(/(\d+)-(\d+)/);
+        Object.assign(this.grid[Number(m[1])][Number(m[2])], entry);
       }
     });
   }
@@ -43,25 +45,12 @@ export default class Home extends Vue {
     return this.yearOf(new Date());
   }
 
-  public weekOf ( date: string | Date ): number {
-    return this.weekOfYear(new Date(date)) -
-      this.weekOfYear(new Date(this.$store.auth.data.dob));
-  }
-
-  public weekOfYear ( d: Date ): number {
-    // adapted from https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
-    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
-    const yearStart: number = new Date(Date.UTC(d.getUTCFullYear(),0,1)).valueOf();
-    return Math.ceil(( ( (d.valueOf() - yearStart) / 86400000) + 1)/7);
-  }
-
   public get weeks ( ): number {
     return Math.floor(new Date().getDate() / 7);
   }
 
-  public createEntry ( ): void {
-    this.$store.http.post("/entries/", {
+  public createEntry ( year: number, week: number ): void {
+    this.$store.http.post(`/entries/${year}-${week}`, {
       content: this.entry
     }).then(( ): void => {
       this.entry = "";
